dea79765e84c78eb16128a39b2355092669f9139
David Miller <david@rasputin.(none)>
2011-04-17 23:38:41 +0100

    Improve Admin interface for items

--- lag/items/admin.py
+++ lag/items/admin.py
@@ -15,7 +15,16 @@ class ItemAdmin(admin.ModelAdmin):
     Interface for editing Items
     """
     inlines = [YNInline]
+    list_display = ['name', 'released', 'quantity']
+    list_filter = ['released']
+
+class TreasureAdmin(ItemAdmin):
+    """
+    Treasure specific interfaces
+    """
+    list_display = ['name', 'category', 'released', 'quantity']
+    list_filter = ['category', 'released']
 
 
 admin.site.register(Artifact, ItemAdmin)
-admin.site.register(Treasure, ItemAdmin)
+admin.site.register(Treasure, TreasureAdmin)
